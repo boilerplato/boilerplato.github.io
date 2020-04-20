b14ad1a8bb8db903651c75470fa78be90d47dbf3
Rousan Ali <rousanali786@gmail.com>
2020-04-21 00:15:26 +0530

    Update CSS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './index.css';
+import imgLogo from '../../media/logo.png';
 
 class Home extends Component {
   constructor(props) {
@@ -13,10 +14,15 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        Home page 5
-        <p>
-          Lorem Ipsum
-        </p>
+        <div className="content">
+          <img className="app-logo" src={imgLogo} alt="Boilerplato Logo" width="128" height="128" />
+          <h1 className="app-title">Boilerplato</h1>
+          <p className="app-desc">A powerful tool to generate boilerplate source code from a template.</p>
+          <div>
+            <a href="https://github.com/boilerplato/boilerplato" target="_blank">GitHub</a>
+            <a className="doc-button" href="https://docs.boilerplato.com/" target="_blank">Getting Started</a>
+          </div>
+        </div>
       </div>
     );
   }
